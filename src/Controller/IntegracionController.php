e26b748a7543981fed2b97da84b314d7c00b6bdc
jonathan789 <jcruz2@udi.edu.co>
2023-01-30 12:27:28 -0500

    poder guardar el envio

--- src/Controller/IntegracionController.php
+++ src/Controller/IntegracionController.php
@@ -177,6 +177,7 @@ class IntegracionController extends AbstractController
             }
             $envio->setEmpresa('DHL');
             $envio->setVerificado(0);
+            $envio->setFacturadoTransportadora(0);
 
             $pais_envio = $doctrine->getRepository(Pais::class)->findOneBy(['code' => $array_envio['shipperDetails']['postalAddress']['countryCode']]);
             $pais_recibe = $doctrine->getRepository(Pais::class)->findOneBy(['code' => $array_envio['receiverDetails']['postalAddress']['countryCode']]);
