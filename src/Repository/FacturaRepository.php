b7e415db4d5847b581009f7db91aaca698fa2e33
ingeniero.jonathan@softwaremedico.com.co <ingeniero.jonathan@softwaremedico.com.co>
2026-03-23 16:46:42 -0500

    datatables

--- src/Repository/FacturaRepository.php
+++ src/Repository/FacturaRepository.php
@@ -55,111 +55,133 @@ class FacturaRepository extends ServiceEntityRepository
 
     public function findByDataTable(array $options = [])
     {
-        $campos = array(
-            "fecha",
-            "numeroFactura",
-            "cliente",
-            "total"
-        );
+        $campos = [
+            0 => 'f.fecha',
+            1 => 'f.numeroFactura',
+            2 => 'c.razonSocial',
+            3 => 'f.total',
+        ];
 
-       
+        $currentPage = $options['page'] ?? 0;
+        $pageSize = $options['pageSize'] ?? 10;
 
-        $currentPage = isset($options['page']) ? $options['page'] : 0;
-        $pageSize = isset($options['pageSize']) ? $options['pageSize'] : 10;
-         
+        $qb = $this->createQueryBuilder('f')
+            ->select([
+                'f.id AS id',
+                'f.fecha AS fecha',
+                'f.numeroFactura AS numeroFactura',
+                'f.total AS total',
+                'f.cufe AS cufe',
+                'f.tipoFactura AS tipoFactura',
+                'f.facturado AS facturado',
+                'f.respuestaDian AS respuestaDian',
+                'f.soportePago AS soportePago',
+                'fr.prefijo AS prefijo',
+                'c.razonSocial AS clienteRazonSocial',
+                'c.nit AS clienteNit',
+            ])
+            ->innerJoin('f.facturaResolucion', 'fr')
+            ->innerJoin('f.cliente', 'c');
 
-        $query = $this->createQueryBuilder('f')
-            ->innerJoin(FacturaResolucion::class, 'fr', Join::WITH,   'fr.id = f.facturaResolucion')
-            ->innerJoin(Clientes::class, 'c', Join::WITH,  'c.id = f.cliente');
-           
-        if($options['nacional'] != 0){
-           
-            $query->andWhere('f.tipoFactura = :tipo')
-                ->setParameter('tipo', 'FACTURA_VENTA_NACIONAL');
+        if (!empty($options['nacional']) && $options['nacional'] != 0) {
+            $qb->andWhere('f.tipoFactura = :tipo')
+            ->setParameter('tipo', 'FACTURA_VENTA_NACIONAL');
         }
-        if ($options['search']) {
-            $shearch = '%' . $options['search'] . '%';
-            $query->andWhere('f.numeroFactura like :val OR f.fecha like :val OR fr.prefijo like :val OR c.razonSocial like :val OR c.nit like :val')
-                ->setParameter('val',$shearch);
-        }
-        $query->andWhere('fr.empresa  = :empresa')
-                ->setParameter('empresa',$options['company']);
 
-        // Procesar múltiples criterios de orden
+        if (!empty($options['search'])) {
+            $search = '%' . $options['search'] . '%';
+            $qb->andWhere('
+                f.numeroFactura LIKE :search OR
+                f.fecha LIKE :search OR
+                fr.prefijo LIKE :search OR
+                c.razonSocial LIKE :search OR
+                c.nit LIKE :search
+            ')
+            ->setParameter('search', $search);
+        }
+
+        $qb->andWhere('fr.empresa = :empresa')
+        ->setParameter('empresa', $options['company']);
+
         if (!empty($options['order'])) {
             foreach ($options['order'] as $order) {
-                $columnIndex = $order['column'];
+                $columnIndex = (int) $order['column'];
                 $direction = strtoupper($order['dir']) === 'ASC' ? 'ASC' : 'DESC';
 
                 if (isset($campos[$columnIndex])) {
-                    $query->addOrderBy("f.{$campos[$columnIndex]}", $direction);
+                    $qb->addOrderBy($campos[$columnIndex], $direction);
                 }
             }
+        } else {
+            $qb->addOrderBy('f.fecha', 'DESC');
         }
 
-        $query->getQuery();
-        $paginator = new Paginator($query);
-        $totalItems = $paginator->count();
-        $paginator->getQuery()->setFirstResult($pageSize * $currentPage)->setMaxResults($pageSize)->getResult();
-        $list = [];
-        foreach ($paginator as $factura) {
+        $countQb = clone $qb;
+        $countQb->select('COUNT(f.id)');
+        $totalItems = (int) $countQb->getQuery()->getSingleScalarResult();
 
+        $rows = $qb->setFirstResult($pageSize * $currentPage)
+            ->setMaxResults($pageSize)
+            ->getQuery()
+            ->getArrayResult();
+
+        $list = [];
+
+        foreach ($rows as $factura) {
             $actions = '';
             $reportar = '';
 
-            
-            $actions .= '<a  class="btn waves-effect waves-light btn-info" href="/impresion/impresion_factura?id=' . $factura->getId() . '" title="Imprimir" target="_blank"><span class="fas fa-print"></span></a>';
-            
-            
-            $actions .= "<button onclick='mostrarNotasCredito(" . $factura->getId() . ");'  class='btn btn-info'>NC</button>";
-            if ($factura->getFacturado()) {
-                $actions .= '<button class="btn btn-success" id="desverificar'.$factura->getId().'" onClick="desverificar('.$factura->getId().');"> <i class="fas fa-check" ></i></button>';
-            }else {
-                $actions .= '<button class="btn btn-secondary" id="verificar'.$factura->getId().'" onClick="verificar('.$factura->getId().');"> <i class="fas fa-check" ></i></button>';
+            $actions .= '<a class="btn waves-effect waves-light btn-info" href="/impresion/impresion_factura?id=' . $factura['id'] . '" title="Imprimir" target="_blank"><span class="fas fa-print"></span></a>';
+
+            $actions .= "<button onclick='mostrarNotasCredito(" . $factura['id'] . ");' class='btn btn-info'>NC</button>";
+
+            if (!empty($factura['facturado'])) {
+                $actions .= '<button class="btn btn-success" id="desverificar'.$factura['id'].'" onClick="desverificar('.$factura['id'].');"><i class="fas fa-check"></i></button>';
+            } else {
+                $actions .= '<button class="btn btn-secondary" id="verificar'.$factura['id'].'" onClick="verificar('.$factura['id'].');"><i class="fas fa-check"></i></button>';
             }
-            if ($factura->getCufe() == '' || $factura->getCufe() == null) {
 
-                
-                $reportar = '<button type="button" id="reportar' . $factura->getId() . '" class="btn" onclick="Reportar(' . $factura->getId() . ');"title="Reportar Dian"><img src="/assets/images/facturas/dian.png" height="30px" width="30px"></button>';
-                  
-                if ($factura->getTipoFactura() == 'FACTURA_VENTA') {
+            if (empty($factura['cufe'])) {
+                $reportar = '<button type="button" id="reportar' . $factura['id'] . '" class="btn" onclick="Reportar(' . $factura['id'] . ');" title="Reportar Dian"><img src="/assets/images/facturas/dian.png" height="30" width="30"></button>';
 
-                    $actions .= '<a  class="btn waves-effect waves-light btn-warning" href="/factura/' . $factura->getId() . '/edit"><span class="fas fa-pencil-alt"></span></a>';
-                } else if ($factura->getTipoFactura() == 'FACTURA_VENTA_NACIONAL') {
-                    $actions .= '<a  class="btn waves-effect waves-light btn-warning" href="/factura_nacionales/' . $factura->getId() . '/edit"><span class="fas fa-pencil-alt"></span></a>';
-                }  else {
-                    $actions .= '<a  class="btn waves-effect waves-light btn-warning" href="/factura_simple/' . $factura->getId() . '/edit"><span class="fas fa-pencil-alt"></span></a>';
+                if ($factura['tipoFactura'] === 'FACTURA_VENTA') {
+                    $actions .= '<a class="btn waves-effect waves-light btn-warning" href="/factura/' . $factura['id'] . '/edit"><span class="fas fa-pencil-alt"></span></a>';
+                } elseif ($factura['tipoFactura'] === 'FACTURA_VENTA_NACIONAL') {
+                    $actions .= '<a class="btn waves-effect waves-light btn-warning" href="/factura_nacionales/' . $factura['id'] . '/edit"><span class="fas fa-pencil-alt"></span></a>';
+                } else {
+                    $actions .= '<a class="btn waves-effect waves-light btn-warning" href="/factura_simple/' . $factura['id'] . '/edit"><span class="fas fa-pencil-alt"></span></a>';
                 }
 
-
-                //$actions .= "<button onclick='mostrarNotasDebito(".$factura->getId().");'  class='btn btn-success'>ND</button>";
-                if ($factura->getRespuestaDian() != '' || $factura->getRespuestaDian() != null) {
-                    $actions .= '<a class="icon-select"  style="position:relative; float:right;cursor:pointer;" onClick="verErrores(' . $factura->getId() . ');" title="Ver respuesta Dian">
-                         <i class="fas fa-code text-success" ></i>
-                     </a>';
+                if (!empty($factura['respuestaDian'])) {
+                    $actions .= '<a class="icon-select" style="position:relative; float:right;cursor:pointer;" onClick="verErrores(' . $factura['id'] . ');" title="Ver respuesta Dian">
+                        <i class="fas fa-code text-success"></i>
+                    </a>';
                 }
             }
-            if($factura->getSoportePago()){
-                $soporte_pago= '<input type="file"  name="archivo_excel" data-id="'.$factura->getId().'" class="form-control file_forma_de_pago"/>  ';
-                $soporte_pago.='<a href="/uploads/images/'.$factura->getSoportePago().'" class="btn btn-dark" target="_blank">
-                <i class="fa fa-clipboard"></i>'.$factura->getSoportePago().'
-            </a>';
-            }else{
-                $soporte_pago= '<input type="file" id="archivo_excel" data-id="'.$factura->getId().'" name="archivo_excel" class="form-control file_forma_de_pago"/>';
-            }
 
+            if (!empty($factura['soportePago'])) {
+                $soporte_pago  = '<input type="file" name="archivo_excel" data-id="'.$factura['id'].'" class="form-control file_forma_de_pago"/>';
+                $soporte_pago .= '<a href="/uploads/images/'.$factura['soportePago'].'" class="btn btn-dark" target="_blank">
+                    <i class="fa fa-clipboard"></i>'.$factura['soportePago'].'
+                </a>';
+            } else {
+                $soporte_pago = '<input type="file" id="archivo_excel" data-id="'.$factura['id'].'" name="archivo_excel" class="form-control file_forma_de_pago"/>';
+            }
 
             $list[] = [
-                'fecha' => $factura->getFecha()->format('Y-m-d'),
-                'numero' => $factura->getFacturaResolucion()->getPrefijo() . $factura->getNumeroFactura(),
-                'cliente' => $factura->getCliente()->getRazonSocial() . '(' . $factura->getCliente()->getNit() . ')',
-                'total' => $factura->getTotal(),
+                'fecha' => $factura['fecha'] instanceof \DateTimeInterface ? $factura['fecha']->format('Y-m-d') : $factura['fecha'],
+                'numero' => $factura['prefijo'] . $factura['numeroFactura'],
+                'cliente' => $factura['clienteRazonSocial'] . ' (' . $factura['clienteNit'] . ')',
+                'total' => $factura['total'],
                 'actions' => $reportar . $actions,
-                'soporte_pago' =>$soporte_pago
+                'soporte_pago' => $soporte_pago,
             ];
-            // echo $item->getZona()->getNombre();
         }
-        return ['data' => $list, 'totalRecords' => $totalItems];
+
+        return [
+            'data' => $list,
+            'totalRecords' => $totalItems,
+        ];
     }
 
 
